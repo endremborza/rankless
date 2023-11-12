65cbc425fdf9b8adfed9f40290894cc22419e99b
Endre Márk Borza <endremborza@gmail.com>
2023-11-12 15:30:51 +0100

    default metric calc

--- src/lib/metric-calculation.ts
+++ src/lib/metric-calculation.ts
@@ -1,21 +1,24 @@
 import type { EntityType } from './constants';
 import { getEntityKind, getNodeByPath } from './tree-functions';
 
-import type { QcSpec, OMap, EmbeddedNode, SpecializationBasis, PathInTree, AttributeLabels, WeightedNode, SpecBaseOptions } from './tree-types'
+import type { QcSpec, SpecializationBasis, PathInTree, AttributeLabels, WeightedNode, SpecBaseOptions } from './tree-types'
 
 export const IGNORED_BASES = [ // too large files...
-    'Institution-Country-Global',
-    'Concept-Continent-Institution',
-    'Institution-Country-Country',
-    'Concept-Country-Institution'
+    'i-Institution-Country-Global',
+    'i-Concept-Continent-Institution',
+    'i-Institution-Country-Country',
+    'i-Concept-Country-Institution'
 ]
 
 export const DEFAULT_SPEC_BASES: Record<EntityType, SpecializationBasis> = {
-    Institution: { basis: 'Continent', hierarchy: 'Global' },
+    Institution: { basis: 'Global', hierarchy: 'Global' },
     Concept: { basis: 'Global', hierarchy: 'Global' },
-    Country: { basis: 'Country', hierarchy: 'Global' },
-    SubConcept: { basis: 'Global', hierarchy: 'Concept' },
-    Continent: { basis: 'Continent', hierarchy: 'Global' }
+    Country: { basis: 'Global', hierarchy: 'Global' },
+    SubConcept: { basis: 'Global', hierarchy: 'Global' },
+    Continent: { basis: 'Global', hierarchy: 'Global' },
+    Period: { basis: 'Global', hierarchy: 'Global' },
+    Year: { basis: 'Global', hierarchy: 'Global' },
+    InstitutionType: { basis: 'Global', hierarchy: 'Global' }
 }
 
 const ID_MAP = {
@@ -38,13 +41,24 @@ export function getSpecMetricObject(
     const node = getNodeByPath(path, weightedRoot);
     const childId = path[path.length - 1]
 
+    const parent = getNodeByPath(path.slice(0, -1), weightedRoot);
+    const siblingCount = Object.keys(parent?.children || {}).length;
+    const normalizerEps = 1 / siblingCount
+    // const nOnLevel = 1; TODO for fallback spec metric
+
+    const rootTypeId = qcSpec.root_entity_type.slice(0, 1).toLowerCase()
 
     let nodeDivisor = weightedRoot.weight;
-    let leafD = specBaselineOptions[specBaseKindToStr(entityKind, baseKind.basis, baseKind.hierarchy)];
-
+    let leafD = specBaselineOptions[specBaseKindToStr(rootTypeId, entityKind, baseKind.basis, baseKind.hierarchy)];
+    if (leafD === undefined) {
+        console.log("cannot find metric for", rootTypeId)
+        const nodeRate = (node?.weight || 0) / nodeDivisor
+        return { nodeRate, nodeDivisor, baselineRate: nodeRate, specMetric: 1 }
+    }
 
     if (baseKind.basis != 'Global') {
-        leafD = leafD[attributeLabels[qcSpec.root_entity_type][rootId].meta[ID_MAP[baseKind.basis] || baseKind.basis]]
+        const attIdKey: number = attributeLabels[qcSpec.root_entity_type][rootId].meta[ID_MAP[baseKind.basis] || baseKind.basis]
+        leafD = leafD[attIdKey]
     }
     if (baseKind.hierarchy != 'Global') {
         for (let i = path.length - 2; i >= 0; i--) {
@@ -70,19 +84,20 @@ export function getSpecMetricObject(
     const baselineRate: number = leafD[childId] || 0
     const nodeRate = (node?.weight || 0) / nodeDivisor
 
+
     return {
-        nodeRate, nodeDivisor, baselineRate, specMetric: nodeRate / baselineRate
+        nodeRate, nodeDivisor, baselineRate, specMetric: (nodeRate + normalizerEps) / (baselineRate + normalizerEps)
     }
 }
 
 
-export function specBaseKindToStr(target: EntityType, basis: string, hierarchy: string) {
+export function specBaseKindToStr(rootTypeId: string, target: EntityType, basis: string, hierarchy: string) {
     // TODO this knows a naming scheme that it shouldn't
-    return `${target}-${basis}-${hierarchy}`
+    return `${rootTypeId}-${target}-${basis}-${hierarchy}`
 }
 
 export function specBaseStrToKind(specBaseStr: string): { target: EntityType } & SpecializationBasis {
-    const [target, basis, hierarchy] = specBaseStr.split('-');
+    const [_, target, basis, hierarchy] = specBaseStr.split('-');
     return { target, basis, hierarchy }
 }
 
