6490324c0c72582a1578d3523a7d58c4ac892d3a
Endre Márk Borza <endremborza@gmail.com>
2024-01-10 16:26:47 +0100

    Updates

---
+++ _app/immutable/chunks/constants.57e5e9c3.js
@@ -0,0 +1,1 @@
+const s="Academic Impact Explorer",t="https://tmp-borza-public-cyx.s3.amazonaws.com/quercus-base/v3.1",a="Institution",c=10;export{s as A,c as D,a as I,t as S};
---
+++ _app/immutable/chunks/style-util.8011bb2c.js
@@ -0,0 +1,1 @@
+function o(r){const n=Math.abs(r-.5)*2;return[r*250,n*220,255-r*250]}function t(r){return`rgb(${o(r).join(", ")})`}export{t as a,o as g};
