c68c799f9b76baf11c060ee87aebf703950ee3a6
Endre Márk Borza <endremborza@gmail.com>
2024-05-08 16:19:31 +0200

    Updates

---
+++ _app/immutable/chunks/navigation.6ec54266.js
@@ -0,0 +1,1 @@
+import{h as t}from"./singletons.c302b54f.js";const o=t("goto"),e=t("after_navigate");export{e as a,o as g};
--- _app/immutable/chunks/navigation.f26de52f.js
+++
@@ -1,1 +0,0 @@
-import{h as t}from"./singletons.2bbb6c66.js";const o=t("goto"),e=t("after_navigate");export{e as a,o as g};
