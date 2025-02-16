d7ea96b7756767d4aec6db9784a838088fe756f2
Endre Márk Borza <endremborza@gmail.com>
2025-02-17 00:45:20 +0100

    midpath align

--- src/lib/components/MidpathBar.svelte
+++ src/lib/components/MidpathBar.svelte
@@ -1,61 +1,15 @@
 <script lang="ts">
-	import type { LevelOutSpec, OMap } from '$lib/tree-types';
+	import { semantify, semOptioned } from '$lib/text-format-util';
+	import type { LevelOutSpec, OMap, RootType } from '$lib/tree-types';
 	import { fade } from 'svelte/transition';
 
 	export let levelSpec: LevelOutSpec;
 	export let index: number;
 	export let selectedBreakdowns: string[];
 	export let totalD1Offset: number;
+	export let d2OffsetCenter: number;
+	export let rootType: RootType;
 	export let dBasedStyle: (d1: OMap<number>, d2: OMap<number>, d3: OMap<number>) => string;
-
-	function semantify(s: string) {
-		let sixPath = selectedBreakdowns[0] == '1-w2qs-1';
-		let aggedPath = selectedBreakdowns[0] == 'agged-institutions-to-countries';
-
-		let globalMap = {
-			'agged-institutions-to-countries': 'working at'
-		};
-
-		let trueInd = aggedPath ? Math.max(index - 1, 0) : index;
-
-		let sMaps = [
-			{
-				'0-concept-hierarchy-0': 'working on <field>',
-				'0-concept-hierarchy-1': 'working on <subfield>',
-				'1-concept-hierarchy-0': 'are cited by papers in <field>',
-				'0-country-hierarchy-0': 'co-author with scholars working in <country>',
-				'1-country-hierarchy-0': 'are cited by authors working in <country>',
-				'0-w2qs-0': 'publish in journals categorized as <q#>',
-				'0-w2qs-1': 'published in <journal>',
-				'1-w2qs-1': 'are cited in papers published in <journal>'
-			},
-			{
-				'1-concept-hierarchy-0': 'and are cited by authors working on',
-				'1-concept-hierarchy-1': 'and in particular',
-				'0-country-hierarchy-1': 'working at',
-				'1-country-hierarchy-0': (sixPath ? '' : 'are cited ') + 'by authors in',
-				'1-country-hierarchy-1': 'at',
-				'0-w2qs-0': 'publish in journals categorized as',
-				'0-w2qs-1': 'such as'
-			},
-			{
-				'0-concept-hierarchy-0': 'focused on',
-				'1-concept-hierarchy-1': 'and in particular in',
-				'1-country-hierarchy-1': 'working at',
-				'1-concept-hierarchy-0': (sixPath ? 'working ' : '') + 'on',
-				'0-w2qs-1': 'such as',
-				'1-w2qs-1': 'published in',
-				'1-country-hierarchy-0': 'and are cited by authors working in'
-			},
-			{
-				'0-concept-hierarchy-1': 'and in particular',
-				'1-concept-hierarchy-0': 'on',
-				'1-concept-hierarchy-1': 'and in particular'
-			},
-			{}
-		];
-		return sMaps[trueInd][s] || globalMap[s] || s;
-	}
 </script>
 
 {#if levelSpec.isVisible}
@@ -71,23 +25,24 @@
 			{}
 		)}
 	>
-		<div class="sel-base sel-cover {levelSpec.levelOptions.length > 1 ? 'sel-clicky' : ''}">
+		<div
+			class="sel-cover {levelSpec.levelOptions.length > 1 ? 'sel-clicky' : ''}"
+			style="--d2-shift: {d2OffsetCenter}%"
+		>
 			<span>
-				{semantify(selectedBreakdowns[index] || '').split('<')[0]}
+				{semantify(selectedBreakdowns[index], rootType, selectedBreakdowns, index).split('<')[0]}
 			</span>
 		</div>
 		{#if levelSpec.levelOptions.length > 1}
 			<select bind:value={selectedBreakdowns[index]} class="sel-base" style="opacity: 0">
 				{#each levelSpec.levelOptions as bd}
 					<option value={bd}>
-						{semantify(bd)}
+						{semOptioned(semantify(bd, rootType, selectedBreakdowns, index), bd)}
 					</option>
 				{/each}
 			</select>
 		{/if}
 		<div class="bg filler" />
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
 	</div>
 {/if}
 
@@ -104,13 +59,14 @@
 	select {
 		cursor: pointer;
 		-webkit-appearance: menulist-button;
+		appearance: menulist-button;
 	}
 
 	.sentenceline {
-		position: fixed;
+		position: absolute;
 		transition: all 700ms;
 		display: flex;
-		justify-content: center;
+		justify-content: left;
 		align-items: center;
 		left: 0%;
 		width: 100%;
@@ -133,22 +89,23 @@
 	.sel-base {
 		position: absolute;
 		top: 0px;
-		left: 0px;
-		width: 100%;
 		height: 100%;
+		width: 100%;
 	}
 
 	.sel-cover {
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		margin-left: var(--d2-shift);
+		transform: translateX(-50%);
 	}
 
 	.sel-clicky > span::after {
 		content: ' \25BD';
 	}
 
-	.sel-base > span {
+	.sel-cover > span {
 		padding: 8px;
 		background: #ffffff70;
 		backdrop-filter: blur(10px);
