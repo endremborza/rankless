aa00a867bb52405ad170f32877c5e302fcaa395e
Endre Márk Borza <endremborza@gmail.com>
2025-02-17 01:08:48 +0100

    link

---
+++ src/lib/components/MaybeLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { entToLink } from '$lib/tree-functions';
+	import type { RootType } from '$lib/tree-types';
+
+	export let semanticId: string;
+	export let name: string;
+	export let rootType: RootType;
+</script>
+
+{#if semanticId.length > 0}
+	<a href={entToLink({ semanticId, rootType })}>{name}</a>
+{:else}
+	{name}
+{/if}
