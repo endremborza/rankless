c386ad1653dc3697b924a2bd687ba5a76404e7d5
Endre Márk Borza <endremborza@gmail.com>
2025-02-17 01:09:04 +0100

    randomlink

---
+++ src/lib/components/RandTreeLink.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { SEMANTIC_CONF, eTypeFromBdDesc, prettifyRoot, semantify } from '$lib/text-format-util';
+	import { getBreakdownOptions, getDefaultYear, toLinkWithParams } from '$lib/tree-functions';
+	import type { RootType, TreeSpecs } from '$lib/tree-types';
+
+	export let name: string;
+	export let semanticId: string;
+	export let rootType: RootType;
+	export let treeSpecs: TreeSpecs;
+
+	function getSem(treeId: number, rootType: RootType, treeSpecs: TreeSpecs) {
+		let bops = getBreakdownOptions(treeSpecs, rootType);
+		let bop = '';
+		for (const [k, v] of Object.entries(bops)) {
+			if (v.treeSpecs.includes(treeId)) {
+				bop = k;
+				break;
+			}
+		}
+		let suff = semantify(bop, rootType, [], 0);
+		let pref = '';
+		if (suff.startsWith('are')) {
+			pref = 'impacted by ';
+			suff = '';
+		} else {
+			suff = ' are ' + suff;
+		}
+
+		let ent = prettifyRoot(eTypeFromBdDesc(bop));
+		return { ent, suff, pref };
+	}
+
+	$: treeId = Math.floor(Math.random() * treeSpecs.specs[rootType].length);
+	$: conf = { year: getDefaultYear(rootType), treeId, semanticId, rootType };
+
+	$: href = toLinkWithParams(conf, {});
+	$: sem = getSem(treeId, rootType, treeSpecs);
+	$: pref = SEMANTIC_CONF[rootType].start;
+</script>
+
+<a {href}>Top {sem.ent} {sem.pref}{pref.toLowerCase()} <b>{name}</b>{sem.suff}</a>
