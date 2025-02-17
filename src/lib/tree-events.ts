b71c3457d0216580b4714fa13ef1e7fd643063c3
Endre Márk Borza <endremborza@gmail.com>
2025-02-17 01:12:25 +0100

    lint

--- src/lib/tree-events.ts
+++ src/lib/tree-events.ts
@@ -1,19 +1,22 @@
-
 import type { InteractionKind, TreeInteractionEvent, PathInTree } from '$lib/tree-types';
 
 import type { EventDispatcher } from 'svelte';
 
-export type EventMap = { 'ti': TreeInteractionEvent };
+export type EventMap = { ti: TreeInteractionEvent };
 type Dispatcher = EventDispatcher<EventMap>;
 
-export function treeInteract(dispatch: Dispatcher, action: InteractionKind, path: PathInTree, x: number, y: number) {
-    return () => {
-        dispatch('ti', {
-            path,
-            action,
-            topLeftCorner: { x, y }
-        });
-    };
+export function treeInteract(
+	dispatch: Dispatcher,
+	action: InteractionKind,
+	path: PathInTree,
+	x: number,
+	y: number
+) {
+	return () => {
+		dispatch('ti', {
+			path,
+			action,
+			topLeftCorner: { x, y }
+		});
+	};
 }
-
-
