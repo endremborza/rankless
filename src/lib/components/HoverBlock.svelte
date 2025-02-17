7ec8ffc2f43bdfee510eab0530dccd02da750cc0
Endre Márk Borza <endremborza@gmail.com>
2025-02-17 01:08:32 +0100

    tmp hover block

---
+++ src/lib/components/HoverBlock.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let show: boolean;
+	export let style: string;
+</script>
+
+{#if show}
+	<div class="hoverover" id="spec-hover" {style}>
+		<slot />
+	</div>
+{/if}
+
+<style>
+	div {
+		padding: 12px;
+		font-size: 0.9rem;
+		position: absolute;
+	}
+</style>
