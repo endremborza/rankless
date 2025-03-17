ce0f9267041e53a32e10678f748c249f041da055
Endre Márk Borza <endremborza@gmail.com>
2025-03-17 12:36:35 +0100

    better link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -202,7 +202,7 @@
 	<div id="main-foot">
 		<TextedLogo pad={0} size={30} />
 		<span>{LATEST_YEAR}</span>
-		<div id="foot-r"><a href={base + '/about#contact'}>Contact</a></div>
+		<div id="foot-r"><a href="{base}/about#contact">Contact</a></div>
 	</div>
 </div>
 
