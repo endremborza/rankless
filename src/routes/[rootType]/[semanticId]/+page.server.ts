78dbd2073b0168ddeff45e54d0cf4fbf0f33126a
Endre Márk Borza <endremborza@gmail.com>
2025-03-31 17:10:02 +0200

    closes #33, closes #34

--- src/routes/[rootType]/[semanticId]/+page.server.ts
+++ src/routes/[rootType]/[semanticId]/+page.server.ts
@@ -1,36 +1,35 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import type * as tt from '$lib/tree-types';
 import * as tf from '$lib/tree-functions';
+import * as lf from '$lib/loading-functions';
 import { BE_URL, FULL_HOST, ROOT_TYPES } from '$lib/constants';
 
 export const load: PageServerLoad = async ({ params, url }) => {
 	let rootType: tt.RootType;
-
 	if (ROOT_TYPES.includes(params.rootType as tt.RootType)) {
 		rootType = params.rootType as tt.RootType;
 	} else {
 		error(404, 'Not found');
 	}
+	let semanticId: string = params.semanticId;
 
-	let semanticId = params.semanticId;
 	let spec: tt.ShareSpec = tf.parseLinkWithParams(url.searchParams, rootType);
+	let conf: tt.FullTreeConfig = { semanticId, year: spec.year, treeId: spec.treeId, rootType };
+	if (semanticId != semanticId.toLowerCase()) {
+		//TODO: should be simple thing to test
+		let linkBase = tf.entToLink({ rootType, semanticId: semanticId.toLowerCase() });
+		let link = tf.decorBaseLink(linkBase, conf, spec.selectionState)
+		redirect(301, link);
+	}
+
 	const view = await fetch(`${BE_URL}/views/${rootType}/${semanticId}`)
 		.then((res) => res.json())
-		.then((view) => view);
+		.then((view) => view)
+		.catch(() => error(404, 'Not found'));
 
-	const treeSpecs: tt.TreeSpecs = await fetch(`${BE_URL}/specs`)
-		.then((res) => res.json())
-		.then((specs: tt.TreeSpecs) => {
-			//possible quick fixes in specs
-			// for (let i = 0; i < specs.specs['sources'].length; i++) {
-			// specs.specs['sources'][i].defaultIsSpec = false;
-			// }
-			return specs
-		});
+	const treeSpecs = await lf.loadSpecs();
 
-
-	let conf: tt.FullTreeConfig = { semanticId, year: spec.year, treeId: spec.treeId, rootType };
 	const treeResp: tt.TreeResponse = await fetch(tf.treeBeUrl(BE_URL, conf, 1))
 		.then((res) => res.json())
 		.then((resp) => resp);
