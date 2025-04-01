81a73808b7b1a2b509095cc9c827d022dfd88635
Endre Márk Borza <endremborza@gmail.com>
2025-04-01 17:37:20 +0200

    webby component

---
+++ src/lib/components/Webby.svelte
@@ -0,0 +1,30 @@
+<a
+	href="https://vote.webbyawards.com/PublicVoting#/2025/websites-and-mobile-sites/general-desktop-mobile-sites/science"
+	target="_blank"
+	class="padded"
+>
+	<span>
+		We are nominated for a Webby! Please vote for us!
+		<br />
+		We are competing against powerful names in Science
+		<!-- <br /> -->
+		<!-- like Quanta Magazine, NY Academy of Sciences, and HHMI -->
+	</span>
+	<img
+		src="https://vote.webbyawards.com/content/img/webbys_logo--white.svg"
+		height="60%"
+		alt="webby logo"
+	/>
+</a>
+
+<style>
+	a {
+		display: flex;
+		gap: 10px;
+		justify-content: space-between;
+		align-items: center;
+		background: var(--color-theme-darkblue);
+		border: solid var(--color-theme-pink) 3px;
+		color: white;
+	}
+</style>
