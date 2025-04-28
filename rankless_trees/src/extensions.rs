75b4046c29d2783acf15332ee70edd2030daabff
Endre Márk Borza <endremborza@gmail.com>
2025-04-29 00:10:12 +0200

    rm flags

--- rankless_trees/src/extensions.rs
+++ rankless_trees/src/extensions.rs
@@ -23,40 +23,7 @@ pub trait DistinctionText: RootInterfaceable + Sized {
 impl DistinctionText for Countries {}
 impl DistinctionText for Subfields {}
 impl DistinctionText for Sources {}
-
-impl DistinctionText for Authors {
-    fn get_distinction_text_arr(
-        rif: &RootInterfaces<Self>,
-        gets: &Getters,
-    ) -> Box<[Option<String>]> {
-        (0..(Self::N + 1))
-            .map(|aid| {
-                let mut ccs = HashMap::new();
-                for irel in rif.inst_rels[aid].iter() {
-                    let cid = gets.icountry(&irel.inst);
-                    if *cid > 0 {
-                        let cc = gets.ccodes(cid);
-                        let entry = ccs.entry(cc.clone()).or_insert(0);
-                        entry.add_assign(irel.papers);
-                    }
-                }
-                let mut kvs: Vec<([u8; 2], u16)> = ccs.into_iter().collect();
-                if kvs.len() > 0 {
-                    kvs.sort_by(|l, r| r.1.cmp(&l.1));
-                    let s = kvs
-                        .iter()
-                        .map(|e| cc_to_flag(&e.0))
-                        .take(4)
-                        .collect::<Vec<String>>()
-                        .join(", ");
-                    Some(s)
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-}
+impl DistinctionText for Authors {}
 
 impl DistinctionText for Institutions {
     fn get_distinction_text_arr(
