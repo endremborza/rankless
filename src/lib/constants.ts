2061de41fece9e3df23dbbf6ec67da3b87f05924
Endre Márk Borza <endremborza@gmail.com>
2025-06-09 21:36:14 +0200

    proper constants for backend api

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,13 @@
 import type { RootType } from './tree-types';
-import { PUBLIC_ORIGIN } from '$env/static/public';
+import { PUBLIC_ORIGIN, PUBLIC_BACKEND_URL } from '$env/static/public';
 
 export const APP_NAME = 'Rankless';
 
-export const FULL_HOST = PUBLIC_ORIGIN || 'http://127.0.0.1:5173';
+export const FULL_HOST = PUBLIC_ORIGIN;
 export const SITEMAP_STEP_SIZE = 1000;
 
 export const BE_URL = 'http://127.0.0.1:3038/v1';
-export const BE_REMOTE_URL = FULL_HOST != 'http://127.0.0.1:5173' ? `${FULL_HOST}:3039/v1` : 'http://127.0.0.1:3039/v1';
+export const BE_REMOTE_URL = `${PUBLIC_BACKEND_URL}/v1`;
 
 export const ROOT_TYPES: RootType[] = [
 	'authors',
