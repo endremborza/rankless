69f4b8e07a7c276fb221e5fa3c3b424ead6ba6d8
Endre Márk Borza <endremborza@gmail.com>
2025-08-04 18:41:10 +0200

    default tree id bug fix

--- src/lib/components/ConceptMap.svelte
+++ src/lib/components/ConceptMap.svelte
@@ -40,7 +40,8 @@
 	let hovered = '0';
 	let hoveredParent: number | undefined = undefined;
 	let toDomains = false;
-	let defaultTreeId = 9; //TODO: anny change can cock this up
+	let defaultTreeId = indsByEntityType.subfields.includes(9) ? 9 : indsByEntityType.subfields[0];
+	//TODO: anny change can cock this up
 
 	let hoveredOverCircle = false;
 	let showPaper = false;
@@ -195,6 +196,7 @@
 	{conf}
 	{treeSpecs}
 	{backupNames}
+	treeId={defaultTreeId}
 	bind:flatOut
 	bind:isSpec
 	bind:showPaper
