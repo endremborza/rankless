d1dae7c0d3767e27aca6fe998b45226b19a688e3
Endre Márk Borza <endremborza@gmail.com>
2025-10-29 13:13:03 +0100

    iter orcid

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -2,7 +2,8 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.user) {
-		throw new Response('Unauthorized', { status: 302, headers: { Location: '/' } });
+		return {}
+		// throw new Response('Unauthorized', { status: 302, headers: { Location: '/' } });
 	}
 	return { user: locals.user };
 };
