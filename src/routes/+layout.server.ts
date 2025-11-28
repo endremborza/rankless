c12c743bbed2b5899cc145e8f1dad6d30c5aa0ea
Endre Márk Borza <endremborza@gmail.com>
2025-11-28 18:15:07 +0100

    rm log

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,6 @@ import type { LayoutServerLoad } from './$types';
 export const trailingSlash = 'never';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
-	console.log("LAYOUT SERVER LOAD RUNNING", url);
 	return {
 		user: locals.user,
 		surveyShouldPrompt: locals.surveyShouldPrompt && !url.pathname.startsWith('/survey')
