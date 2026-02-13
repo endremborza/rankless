b5f1929cd04cee879820d7d846a10268a9c87a09
Endre Márk Borza <endremborza@gmail.com>
2026-02-13 18:52:12 +0100

    global cache

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,14 @@
 import { writable } from 'svelte/store';
+import type { OaPaperResp } from '$lib/tree-types';
 
 export const resultsHidden = writable(true);
+
+const paperCache = new Map<number, OaPaperResp>();
+
+export function getCachedPaper(id: number) {
+	return paperCache.get(id);
+}
+
+export function setCachedPaper(id: number, data: OaPaperResp) {
+	paperCache.set(id, data);
+}
