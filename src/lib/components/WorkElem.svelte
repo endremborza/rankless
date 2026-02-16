75a625c04ec6acb4df6f69a0ae3d60265fbe8b1a
Endre Márk Borza <endremborza@gmail.com>
2026-02-16 14:09:33 +0100

    failed load catch

--- src/lib/components/WorkElem.svelte
+++ src/lib/components/WorkElem.svelte
@@ -10,6 +10,7 @@
 	export let attributeLabels: AttributeLabels;
 	export let instId: number | undefined;
 
+	let placeholder = 'Loading...';
 	let title = '';
 	let doi = '';
 	let abstract = '';
@@ -40,6 +41,7 @@
 	$: instName = instAtts.name || '';
 	$: fullInstName = instName.length > 50 ? 'affiliated' : `from ${instName}`;
 	$: oaLink = `https://openalex.org/works/W${workId}`;
+	$: errMsg = `Failed to load top paper from OpenAlex <a href="${oaLink}">link</a>`;
 	$: href = doi.length > 0 ? doi : oaLink;
 
 	$: parsePaperResp(paperResp);
@@ -70,35 +72,44 @@
 	onMount(() => {
 		if (workId == 0 || paperResp != undefined) return;
 		let oaUrl = `https://api.openalex.org/works/W${workId}?select=publication_year,title,doi,authorships,abstract_inverted_index`;
-		fetch(oaUrl).then((resp) => {
-			resp.json().then((o) => {
-				let aWords = [];
-				for (const [word, idxs] of Object.entries(o.abstract_inverted_index || {})) {
-					for (const i of idxs) {
-						aWords[i] = word;
-					}
-				}
-				let abstract = aWords.join(' ');
-				let authors = [];
-				for (let aship of o.authorships) {
-					let institutions = [];
-					for (let aff of aship.institutions || []) {
-						institutions.push(aff.id);
-					}
-					let lElems = aship.author.id.split('/');
-					let link = `/oa-id/${lElems[lElems.length - 1]}`;
-					authors.push({ name: aship.author.display_name, link, institutions });
-				}
-				paperResp = {
-					title: o.title,
-					doi: o.doi || '',
-					year: o.publication_year,
-					abstract,
-					authors
-				};
-				setCachedPaper(workId, paperResp);
+		fetch(oaUrl)
+			.then((resp) => {
+				resp
+					.json()
+					.then((o) => {
+						let aWords = [];
+						for (const [word, idxs] of Object.entries(o.abstract_inverted_index || {})) {
+							for (const i of idxs) {
+								aWords[i] = word;
+							}
+						}
+						let abstract = aWords.join(' ');
+						let authors = [];
+						for (let aship of o.authorships) {
+							let institutions = [];
+							for (let aff of aship.institutions || []) {
+								institutions.push(aff.id);
+							}
+							let lElems = aship.author.id.split('/');
+							let link = `/oa-id/${lElems[lElems.length - 1]}`;
+							authors.push({ name: aship.author.display_name, link, institutions });
+						}
+						paperResp = {
+							title: o.title,
+							doi: o.doi || '',
+							year: o.publication_year,
+							abstract,
+							authors
+						};
+						setCachedPaper(workId, paperResp);
+					})
+					.catch(() => {
+						placeholder = errMsg;
+					});
+			})
+			.catch(() => {
+				placeholder = errMsg;
 			});
-		});
 	});
 </script>
 
@@ -152,13 +163,16 @@
 		</footnote>
 	</div>
 {:else}
-	<div class="loading-sign"><h3>Loading...</h3></div>
+	<div class="loading-sign"><h4>{@html placeholder}</h4></div>
 {/if}
 
 <style>
 	h2,
 	h4 {
 		margin: calc(var(--unified-padding) / 2);
+	}
+	h4 {
+		text-align: center;
 	}
 
 	a {
