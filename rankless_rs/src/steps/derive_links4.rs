3e002ea151efc9b9098807ea1143c9fcac408873
Endre Márk Borza <endremborza@gmail.com>
2026-02-24 12:49:20 +0100

    add author impact tree hits

--- rankless_rs/src/steps/derive_links4.rs
+++ rankless_rs/src/steps/derive_links4.rs
@@ -1,4 +1,6 @@
+use hashbrown::HashSet;
 use std::{io, sync::Arc};
+use tqdm::Iter;
 
 use dmove::{
     para_multi_gen_run, BigId, Entity, LoadedIdMap, MarkedAttribute, NamespacedEntity,
@@ -9,14 +11,16 @@ use crate::{
     common::{EmptyAttributeEntity, HitWorkMarker, MainWorkMarker},
     gen::{
         a1_entity_mapping::{Authors, Countries, Institutions, Sources, Subfields, Topics, Works},
+        a2_init_atts::WorkReferences,
+        derive_links1::WorkFilteredAuthors,
         derive_links2::WorkCitingCounts,
         derive_links3::{
             HitPapers, HitPapersCiteCounts, HitPapersDois, HitPapersNames, HitPapersWids,
         },
     },
     steps::derive_links3::work_count,
-    CiteCountMarker, NameExtensionMarker, NameMarker, QuickestBox, QuickestNumbered, ReadIter,
-    SemanticIdMarker, Stowage,
+    CiteCountMarker, NameExtensionMarker, NameMarker, QuickestBox, QuickestNumbered, QuickestVBox,
+    ReadIter, SemanticIdMarker, Stowage,
 };
 
 fn sorted_hit_papers<E>(
@@ -72,11 +76,64 @@ impl MarkedAttribute<NameExtensionMarker> for HitPapers {
 
 pub fn main(stowage: Stowage) -> io::Result<()> {
     work_count::<Countries>(&stowage);
-    let hit_map = stowage.get_entity_interface::<HitPapers, QuickestNumbered>(); //TODO: whis can
+    let hit_map = stowage.get_entity_interface::<HitPapers, QuickestNumbered>(); //TODO: this can
                                                                                  //try and fail with QuickMap
     let wcc = stowage.get_entity_interface::<WorkCitingCounts, QuickestBox>();
+    let wor_refs = stowage.get_entity_interface::<WorkReferences, QuickestVBox>();
+    let w2a = stowage.get_entity_interface::<WorkFilteredAuthors, QuickestVBox>();
+
     let parc = Arc::new((stowage, hit_map, wcc));
     para_multi_gen_run!(sorted_hit_papers, Institutions, Authors, Countries, Sources, Subfields, Topics; parc).last();
+
+    let mut direct: Vec<HashSet<ET<HitPapers>>> = vec![HashSet::new(); Authors::N];
+    let mut once_removed: Vec<HashSet<ET<HitPapers>>> = vec![HashSet::new(); Authors::N];
+
+    for (&hp_wid_big, &hp_id) in parc.1 .0.iter().tqdm().desc(Some("hit papers for authors")) {
+        let hp_wid = hp_wid_big.to_usize();
+        let hp_authors: HashSet<ET<Authors>> = w2a.0[hp_wid].iter().copied().collect();
+
+        for &ref_wid in wor_refs.0[hp_wid].iter() {
+            let ru = ref_wid.to_usize();
+            let ref_authors = &w2a.0[ru];
+            for &aid in ref_authors.iter() {
+                if !hp_authors.contains(&aid) {
+                    direct[aid.to_usize()].insert(hp_id);
+                }
+            }
+            for &ref2_wid in wor_refs.0[ru].iter() {
+                let r2u = ref2_wid.to_usize();
+                let ref2_authors = &w2a.0[r2u];
+                for &aid in ref2_authors.iter() {
+                    if !hp_authors.contains(&aid)
+                        && !ref_authors.contains(&aid)
+                        && !direct[aid.to_usize()].contains(&hp_id)
+                    {
+                        once_removed[aid.to_usize()].insert(hp_id);
+                    }
+                }
+            }
+        }
+    }
+
+    let to_sorted = |sets: Vec<HashSet<ET<HitPapers>>>| {
+        sets.into_iter().map(|s| {
+            let mut v: Vec<ET<HitPapers>> = s.into_iter().collect();
+            v.sort();
+            v.into_boxed_slice()
+        })
+    };
+
+    let n_direct: usize = direct.iter().map(|s| s.len()).sum();
+    let n_once: usize = once_removed.iter().map(|s| s.len()).sum();
+    println!("direct: {n_direct} total entries, once_removed: {n_once} total entries");
+    parc.0.add_iter_owned::<VarAttBuilder, _, _>(
+        to_sorted(direct),
+        Some("author-citing-hits-direct"),
+    );
+    parc.0.add_iter_owned::<VarAttBuilder, _, _>(
+        to_sorted(once_removed),
+        Some("author-citing-hits-once"),
+    );
     parc.0.write_code()?;
     Ok(())
 }
