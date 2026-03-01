39e613ea366d66ef45505606826d3f735ca0adb7
Endre Márk Borza <endremborza@gmail.com>
2026-03-01 19:05:25 +0100

    forward return to data

--- src/routes/(stat)/login/+server.ts
+++ src/routes/(stat)/login/+server.ts
@@ -2,12 +2,14 @@ import { ORCID_CLIENT_ID } from '$env/static/private';
 import { ORCID_REDIRECT_URI, ORCID_AUTH_URL } from '$lib/constants';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async ({ url }) => {
+	const returnTo = url.searchParams.get('returnTo') ?? '/';
 	const params = new URLSearchParams({
 		client_id: ORCID_CLIENT_ID,
 		response_type: 'code',
 		scope: '/authenticate',
-		redirect_uri: ORCID_REDIRECT_URI
+		redirect_uri: ORCID_REDIRECT_URI,
+		state: returnTo
 	});
 
 	return new Response(null, {
