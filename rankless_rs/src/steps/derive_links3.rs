82a6feca82416e81249407b8fc9c7c9696a0f937
Endre Márk Borza <endremborza@gmail.com>
2026-03-05 14:48:12 +0100

    init moving stuff out from server startup

--- rankless_rs/src/steps/derive_links3.rs
+++ rankless_rs/src/steps/derive_links3.rs
@@ -1,15 +1,23 @@
-use std::{collections::BinaryHeap, ops::AddAssign, sync::Arc};
+use std::{cmp::Ordering, collections::BinaryHeap, ops::AddAssign, sync::Arc};
 
 use crate::{
-    common::{init_empty_slice, MainWorkMarker},
+    common::{
+        init_empty_slice, reverse_id, CoordinateMarker, MainWorkMarker, NameMarker,
+        PageFilterMarker, PeerAuthorMarker, Top3CitingSfMarker, Top3PaperSfMarker,
+        YearlyPapersMarker,
+    },
     env_consts::FINAL_YEAR,
     gen::{
-        a1_entity_mapping::{Authors, Institutions, Sources, Subfields, Topics, Works},
+        a1_entity_mapping::{Authors, Countries, Institutions, Sources, Subfields, Topics, Works},
         a2_init_atts::{AuthorNobels, WorkDois, WorkTopics, WorkYears, WorksNames},
         derive_links1::{WorkFilteredAuthors, WorkSubfields},
-        derive_links2::AuthorWorks,
+        derive_links2::{AuthorWorks, SourceStats},
+        derive_links3::HitPapers,
     },
-    steps::a1_entity_mapping::{YearInterface, Years},
+    steps::{
+        a1_entity_mapping::{YearInterface, Years},
+        derive_links2::Top3Rec,
+    },
     CiteCountMarker, QuickestBox, QuickestVBox, ReadIter, Stowage, WorkCountMarker,
 };
 use dmove::{
@@ -29,8 +37,228 @@ const W_YEAR: f64 = 0.1;
 const W_SF_YEAR: f64 = 1.0 - W_SF - W_YEAR;
 const SCORE_THRESHOLD: f64 = 3.0;
 const NOBEL_MULTIPLIER: f64 = 2.0;
+const COORD_MIN_CITES: f64 = 1.0;
+const COORD_MIN_PAPERS: f64 = 3.0;
+const N_PEERS: usize = 5;
+const N_COORD_CANDIDATES: usize = 1000;
+
+pub const AUTHOR_BLACKLIST: [u64; 3] = [
+    5030786976, //Lynnette Nathalie Lyzwinski
+    5036138197, //David S. Gokhin
+    5034807195, //Shadi Yarandi
+];
 
 type CCUI = ET<MAA<Works, CiteCountMarker>>;
+
+struct CoordCandidate {
+    sq_dist: f64,
+    idx: usize,
+}
+
+struct PeerCandidate {
+    neg_similarity: i32,
+    coord_dist_sq: f64,
+    dm_id: u16,
+}
+
+impl PartialEq for CoordCandidate {
+    fn eq(&self, other: &Self) -> bool {
+        self.sq_dist == other.sq_dist
+    }
+}
+impl Eq for CoordCandidate {}
+impl PartialOrd for CoordCandidate {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for CoordCandidate {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.sq_dist
+            .partial_cmp(&other.sq_dist)
+            .unwrap_or(Ordering::Equal)
+    }
+}
+
+impl PartialEq for PeerCandidate {
+    fn eq(&self, other: &Self) -> bool {
+        self.neg_similarity == other.neg_similarity && self.coord_dist_sq == other.coord_dist_sq
+    }
+}
+impl Eq for PeerCandidate {}
+impl PartialOrd for PeerCandidate {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for PeerCandidate {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.neg_similarity.cmp(&other.neg_similarity).then(
+            self.coord_dist_sq
+                .partial_cmp(&other.coord_dist_sq)
+                .unwrap_or(Ordering::Equal),
+        )
+    }
+}
+
+macro_rules! entity_coords_filter {
+    ($stowage:expr, $E:ty, |$i:ident, $c:ident, $p:ident| $extra:expr) => {{
+        let cites = $stowage.get_marked_interface::<$E, CiteCountMarker, QuickestBox>();
+        let wcounts = $stowage.get_marked_interface::<$E, WorkCountMarker, QuickestBox>();
+        let n = cites.len();
+        let mut coords = Vec::with_capacity(n);
+        let mut filter = Vec::with_capacity(n);
+        for ($i, name) in $stowage
+            .get_entity_interface::<MAA<$E, NameMarker>, ReadIter>()
+            .enumerate()
+        {
+            let $c = cites[$i].to_usize();
+            let $p = wcounts[$i].to_usize();
+            let cf = $c as f64;
+            coords.push([
+                f64::max(cf, COORD_MIN_CITES).ln(),
+                cf / f64::max($p as f64, COORD_MIN_PAPERS),
+            ]);
+            let base = name.trim().len() > 0 && $p > 1 && $c > 2;
+            filter.push(if base && { $extra } { 1u8 } else { 0u8 });
+        }
+        $stowage.ditf::<CoordinateMarker, $E, _>(coords.clone(), "coordinates");
+        $stowage.ditf::<PageFilterMarker, $E, _>(filter.clone(), "page-filter");
+        (coords, filter)
+    }};
+}
+
+fn sf_overlap(a: &Top3Rec<Subfields>, b: &Top3Rec<Subfields>) -> u32 {
+    let mut count = 0u32;
+    for &(_, aid) in a {
+        if aid == 0 {
+            continue;
+        }
+        for &(_, bid) in b {
+            if aid == bid {
+                count += 1;
+                break;
+            }
+        }
+    }
+    count
+}
+
+fn compute_author_peers(stowage: &Stowage, raw_coords: &[[f64; 2]], filter: &[u8]) {
+    let citing_sfs = stowage.get_marked_interface::<Authors, Top3CitingSfMarker, QuickestBox>();
+    let paper_sfs = stowage.get_marked_interface::<Authors, Top3PaperSfMarker, QuickestBox>();
+
+    let mut entries: Vec<(usize, [f64; 2])> = filter
+        .iter()
+        .enumerate()
+        .filter(|(_, &f)| f > 0)
+        .map(|(i, _)| (i, raw_coords[i]))
+        .collect();
+
+    let n = entries.len() as f64;
+    let mut means = [0.0f64; 2];
+    for (_, c) in &entries {
+        means[0] += c[0] / n;
+        means[1] += c[1] / n;
+    }
+    let mut vars = [0.0f64; 2];
+    for (_, c) in &entries {
+        vars[0] += (c[0] - means[0]).powi(2) / n;
+        vars[1] += (c[1] - means[1]).powi(2) / n;
+    }
+    let stds = [vars[0].sqrt().max(1e-10), vars[1].sqrt().max(1e-10)];
+    for (_, c) in &mut entries {
+        c[0] = (c[0] - means[0]) / stds[0];
+        c[1] = (c[1] - means[1]) / stds[1];
+    }
+
+    entries.sort_by(|a, b| a.1[0].partial_cmp(&b.1[0]).unwrap_or(Ordering::Equal));
+
+    let mut peers = vec![[0u16; N_PEERS]; raw_coords.len()];
+    for si in 0..entries.len() {
+        let (dm_id, ref_coord) = entries[si];
+        let mut coord_heap: BinaryHeap<CoordCandidate> = BinaryHeap::new();
+        let mut lo = si as isize - 1;
+        let mut hi = si + 1;
+
+        loop {
+            let lo_valid = lo >= 0;
+            let hi_valid = hi < entries.len();
+            if !lo_valid && !hi_valid {
+                break;
+            }
+            let lo_d0 = if lo_valid {
+                (entries[lo as usize].1[0] - ref_coord[0]).abs()
+            } else {
+                f64::MAX
+            };
+            let hi_d0 = if hi_valid {
+                (entries[hi].1[0] - ref_coord[0]).abs()
+            } else {
+                f64::MAX
+            };
+            let (cand_si, is_lo) = if lo_d0 <= hi_d0 {
+                (lo as usize, true)
+            } else {
+                (hi, false)
+            };
+
+            let d0 = entries[cand_si].1[0] - ref_coord[0];
+            let d0_sq = d0 * d0;
+            let threshold = if coord_heap.len() >= N_COORD_CANDIDATES {
+                coord_heap.peek().unwrap().sq_dist
+            } else {
+                f64::MAX
+            };
+            if d0_sq > threshold {
+                break;
+            }
+
+            let d1 = entries[cand_si].1[1] - ref_coord[1];
+            let sq_dist = d0_sq + d1 * d1;
+            if sq_dist < threshold {
+                if coord_heap.len() >= N_COORD_CANDIDATES {
+                    coord_heap.pop();
+                }
+                coord_heap.push(CoordCandidate {
+                    sq_dist,
+                    idx: cand_si,
+                });
+            }
+
+            if is_lo {
+                lo -= 1;
+            } else {
+                hi += 1;
+            }
+        }
+
+        let mut peer_heap: BinaryHeap<PeerCandidate> = BinaryHeap::new();
+        for cc in coord_heap.into_iter() {
+            let (cand_dm_id, _) = entries[cc.idx];
+            let sim = sf_overlap(&citing_sfs[dm_id], &citing_sfs[cand_dm_id])
+                + sf_overlap(&paper_sfs[dm_id], &paper_sfs[cand_dm_id]);
+            let pc = PeerCandidate {
+                neg_similarity: -(sim as i32),
+                coord_dist_sq: cc.sq_dist,
+                dm_id: cand_dm_id as u16,
+            };
+            if peer_heap.len() < N_PEERS || pc < *peer_heap.peek().unwrap() {
+                if peer_heap.len() >= N_PEERS {
+                    peer_heap.pop();
+                }
+                peer_heap.push(pc);
+            }
+        }
+
+        for (pi, pc) in peer_heap.into_sorted_vec().into_iter().enumerate() {
+            peers[dm_id][pi] = pc.dm_id;
+        }
+    }
+
+    println!("computed peers for {} filtered authors", entries.len());
+    stowage.ditf::<PeerAuthorMarker, Authors, _>(peers, "peers");
+}
 
 pub fn work_count<E>(stowage: &Stowage)
 where
@@ -65,7 +293,7 @@ pub fn get_nobeled_works(stowage: &Stowage, w_years: &[ET<Years>]) -> HashSet<ET
 
 pub fn main(stowage: Stowage) -> std::io::Result<()> {
     let starc = Arc::new(stowage);
-    para_multi_gen_run!(work_count, Sources, Institutions, Authors, Subfields, Topics; starc)
+    para_multi_gen_run!(work_count, Sources, Institutions, Authors, Subfields, Topics, Countries; starc)
         .last();
 
     let cc_interface = starc.get_entity_interface::<MAA<Works, CiteCountMarker>, QuickestBox>();
@@ -159,6 +387,16 @@ pub fn main(stowage: Stowage) -> std::io::Result<()> {
         .collect();
 
     starc.add_iter_owned::<Data64MappedEntityBuilder, _, _>(hit_papers, Some("hit-papers"));
+
+    let hit_coords: Vec<[f64; 2]> = hit_ccounts
+        .iter()
+        .map(|&cc| {
+            let cf = f64::max(cc as f64, COORD_MIN_CITES);
+            [cf.ln(), cf / COORD_MIN_PAPERS]
+        })
+        .collect();
+    let hit_filter: Vec<u8> = vec![1u8; hit_ccounts.len()];
+
     starc.add_iter_owned::<VarAttBuilder, _, _>(hit_names.into_iter(), Some("hit-papers-names"));
     starc.add_iter_owned::<VarAttBuilder, _, _>(hit_dois.into_iter(), Some("hit-papers-dois"));
     starc.add_iter_owned::<VarAttBuilder, _, _>(hit_wids.into_iter(), Some("hit-papers-wids"));
@@ -167,6 +405,31 @@ pub fn main(stowage: Stowage) -> std::io::Result<()> {
         hit_ccounts.into_iter(),
         Some("hit-papers-cite-counts"),
     );
+
+    entity_coords_filter!(starc, Institutions, |_i, _c, _p| true);
+    entity_coords_filter!(starc, Subfields, |_i, _c, _p| true);
+    entity_coords_filter!(starc, Countries, |_i, _c, _p| true);
+
+    let source_stats = starc.get_entity_interface::<SourceStats, QuickestBox>();
+    entity_coords_filter!(starc, Sources, |i, c, p| {
+        p > 10 && c > 20 && source_stats[i].1 <= 2
+    });
+
+    let author_yearly_papers =
+        starc.get_marked_interface::<Authors, YearlyPapersMarker, QuickestBox>();
+    let author_oa_ids = reverse_id::<Authors>(&starc);
+    let (author_coords, author_filter) = entity_coords_filter!(starc, Authors, |i, _c, p| {
+        p < 10_000
+            && *author_yearly_papers[i].iter().max().unwrap_or(&0) < 300
+            && !AUTHOR_BLACKLIST.contains(&author_oa_ids[i])
+    });
+
+    starc.ditf::<CoordinateMarker, HitPapers, _>(hit_coords, "coordinates");
+    starc.ditf::<PageFilterMarker, HitPapers, _>(hit_filter, "page-filter");
+
+    println!("computing author peers");
+    compute_author_peers(&starc, &author_coords, &author_filter);
+
     starc.write_code()?;
     Ok(())
 }
