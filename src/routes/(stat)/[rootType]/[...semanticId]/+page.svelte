3f086bd7092215b109d13b276c17ff676ce7e41b
Endre Márk Borza <endremborza@gmail.com>
2026-03-07 01:35:35 +0100

    stop hiding

--- src/routes/(stat)/[rootType]/[...semanticId]/+page.svelte
+++ src/routes/(stat)/[rootType]/[...semanticId]/+page.svelte
@@ -147,7 +147,7 @@
 				fullHeight={ticksHeight}
 			/>
 		</div>
-		{#if data.conf.rootType === 'authors-hidden'}
+		{#if data.conf.rootType === 'authors'}
 			<a href="/author-papers/{data.conf.semanticId}" class="explore-card shadowy marged padded">
 				<div>
 					<h3>Paper Profile & Citation Paths</h3>
