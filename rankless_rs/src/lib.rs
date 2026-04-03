c3d5efc27d6fc939d6d4ffc2503207facdf85266
Endre Márk Borza <endremborza@gmail.com>
2026-04-03 14:44:17 +0200

    line write as dmove_macro

--- rankless_rs/src/lib.rs
+++ rankless_rs/src/lib.rs
@@ -47,12 +47,4 @@ pub fn runner(comm: &str, root_str: &str, in_root_o: Option<String>) -> io::Resu
     }
     subrun(comm, stowage)
 }
-mods_as_comms!(
-    a1_entity_mapping,
-    a2_init_atts,
-    derive_links1,
-    derive_links2,
-    derive_links3,
-    derive_links4,
-    derive_links5
-);
+mods_as_comms!(a1_entity_mapping, a2_init_atts, derive_links1, derive_links2, derive_links3, derive_links4, derive_links5);
